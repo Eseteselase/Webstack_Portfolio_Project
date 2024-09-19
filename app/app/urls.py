c085146df67cb80c07eec6f842b900587ccc23b8
Eseteselase <esete.s.berhanu@gmail.com>
2024-09-19 11:49:54 +0300

    url edit

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,7 +1,5 @@
-"""app URL Configuration
+"""Esete app URL Configuration
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
